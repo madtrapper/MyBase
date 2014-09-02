53a1539017ee0a41f810ee0e4ee4f23aff57858e
unknown <wwn518@example.com>
2014-09-01 22:47:40 -0700

    Adjust project setting.

--- unit_test/total_test/total_test.cpp
+++ unit_test/total_test/total_test.cpp
@@ -3,21 +3,25 @@
 
 #include "stdafx.h"
 #include <conio.h>
+#include "base/at_exit.h"
 #include "base/basictypes.h"
 #include "base/command_line.h"
 #include "base/process/launch.h"
 #include "base/files/file_util.h"
 #include "base/files/file_enumerator.h"
+#include "base/message_loop/message_loop.h"
+
+static base::AtExitManager exit_manager;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	base::MessageLoop mainLoop;
 	CommandLine cl(argc, argv);
 	
 	base::FilePath exePath;
 	wchar_t szExeName[MAX_PATH];
 
 	::GetModuleFileName(NULL, szExeName, MAX_PATH);	
-	
 
 	if (cl.HasSwitch("server")) {
 		printf("Server mode.\n");
@@ -35,10 +39,14 @@ int _tmain(int argc, _TCHAR* argv[])
 		base::FilePath scan_dir = cl.GetSwitchValuePath("d");
 		_tprintf(L"Target : %s\n", scan_dir.AsUTF16Unsafe().c_str());
 
-		base::FileEnumerator scan_enum(scan_dir, true, base::FileEnumerator::FILES,
+		base::FileEnumerator scan_enum(scan_dir, 
+									   true, 
+									   base::FileEnumerator::FILES,
 									   FILE_PATH_LITERAL("*.*"));
 		int total_file_count = 0;
-		for (base::FilePath name = scan_enum.Next(); !name.empty(); name = scan_enum.Next(), total_file_count++) {
+		for (base::FilePath name = scan_enum.Next(); 
+			!name.empty(); 
+			name = scan_enum.Next(), total_file_count++) {
 			_tprintf(L"File : %s\n", name.AsUTF16Unsafe().c_str());
 		}
 
@@ -52,6 +60,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		printf("cmd error\n");
 	}
 
+	printf("Press enter to exit.\n");
 	_getch();
 
 	return 0;
